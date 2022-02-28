b4a4e6b71f92093d167318890881b0fb007fa617
m.bagri <1920160159>
2022-02-28 10:24:41 +0330

    update project

--- src/views/survey.blade.php
+++ src/views/survey.blade.php
@@ -132,6 +132,6 @@
   </div>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+<script src="{{__DIR__.'bootstrap.min.js'}}"></script>
 </body>
 </html>
